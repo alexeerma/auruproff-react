3b8f3d57a57a456e92a97f880e52de60a68f8cd6
alexeerma <aleksander.eerma@hotmail.com>
2023-04-11 15:25:53 +0300

    Added Router logic and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+
+
+import {NavBar} from './components';
+import {Home, Survepesu, Siselahendused, Work, People, FAQ, Contact} from './Pages';
+
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+        <>
+          <NavBar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/survepesu" component={Survepesu} />
+            <Route path='/siselahendused' component={Siselahendused} />
+            <Route path='/tood' component={Work} />
+            <Route path='/inimesed' component={People} />
+            <Route path='/kkk' component={FAQ} />
+            <Route path='/kontakt' component={Contact} />
+          </Switch>
+        </>
+      );
+    }
 
 export default App

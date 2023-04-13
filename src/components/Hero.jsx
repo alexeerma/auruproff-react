9efd31ca667ccced386e6357db50e41b9bedb897
alexeerma <aleksander.eerma@hotmail.com>
2023-04-13 19:29:32 +0300

    Hero section primitive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,14 @@ import React from 'react';
 const Hero = () => (
   
   <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center h-screen'>
-    <div className='card-container'>
-      <div>
-        <h1>Sinu professionaalne eripuhastustööde partner</h1>
-          <ul>
-            <li>Tänavakivi, katuse ja fassaadi puhastus</li>
-            <li>Tööstusseadmete ja -pindade süvapuhastus</li>
-            <li>Lao-, kaubandus- ja äripindade ning spordiklubide ja ujulate puhastus</li>
-            <li>Eritööd (sh põranda ja vaipkatete süvapesu, vahatamine, pindade kaitsetöötlus jpm)</li>
+    <div className=''>
+      <div className='pl-[100px] pt-[300px]'>
+        <h1 className='font-poppins text-6xl text-white w-[700px]' >Sinu professionaalne eripuhastustööde partner</h1>
+          <ul className='list-disc pl-5 pt-5 text-white' >
+            <li className='py-2'>Tööstusseadmete ja -pindade süvapuhastus</li>
+            <li className='py-2'>Tänavakivi, katuse ja fassaadi puhastus</li>
+            <li className='py-2'>Lao-, kaubandus- ja äripindade ning spordiklubide ja ujulate puhastus</li>
+            <li className='py-2'>Eritööd (sh põranda ja vaipkatete süvapesu, vahatamine, pindade kaitsetöötlus jpm)</li>
           </ul>
       </div>
     </div>

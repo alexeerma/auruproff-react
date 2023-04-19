0fbe95d9c54cd893b1c083a1d72f4a058231e81a
alexeerma <aleksander.eerma@hotmail.com>
2023-04-19 23:04:42 +0300

    Managed the layout, image needs fixing

--- src/components/Solutions.jsx
+++ src/components/Solutions.jsx
@@ -12,15 +12,27 @@ const Solutions = () => (
         <span className="h-1 w-full p-0.5 text-primary text-xl font-bold lg:w-1/3 ">_____</span>
         <p className="py-5 text-primary text-lg">Auruproff teeb eripuhastustöid nii sise- kui välitingimustes. Siit leiad laia valiku teenuseid puhtama keskkonna loomiseks. Töötame iga päev selle nimel, et olla meie klientide soovide ja vajaduste kõrgusel ning muuta puhastus ettevõtete jaoks võimalikult stressivabaks!</p>
       </div>
-      <div className='w-1/3 relative flex flex-col'>
-        <img src={siselahendused_pic} alt="" className='' />
-        <h1 className='text-center text-primary font-poppins font-bold text-2xl uppercase'>Siselahendused</h1>
-        <CTAButton2 className="w-[200px]" />
+      <div className='w-1/3 relative flex flex-col justify-items-center items-center'>
+        <div>
+          <img src={siselahendused_pic} alt="" className='' /> 
+        </div>
+        <div>
+          <h1 className='text-center text-primary font-poppins font-bold text-2xl uppercase'>Siselahendused</h1>
+        </div>
+        <div>
+          <CTAButton2 className="w-[200px]" />
+        </div>
       </div>
-      <div className='w-1/3 relative'>
-        <img src={valilahendused_pic} alt="" className='object-contain' />
+      <div className='w-1/3 relative flex flex-col justify-items-center items-center'>
+        <div>
+          <img src={valilahendused_pic} alt="" className='object-contain' />
+        </div>
+        <div>
         <h1 className='text-center text-primary font-poppins font-bold text-2xl uppercase'>Välilahendused</h1>
-        <CTAButton2/>
+        </div>
+        <div>
+          <CTAButton2/>
+        </div>  
       </div>
     </div>
   </div>

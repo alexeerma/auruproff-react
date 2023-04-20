8799eb61905a67967a659812e7a12c6e80591c7d
alexeerma <aleksander.eerma@hotmail.com>
2023-04-20 09:51:37 +0300

    Star of working on footer contactform

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => (
 
   <section className='bg-contact-pattern h-[500px] flex flex-col justify-center justify-items-center items-center'>
     
-      <div className='text-white text-4xl font-poppins font-medium pb-7'>PERSONAALNE • PÕHJALIK • PAINDLIK</div>
+      <div className='text-white text-xl font-poppins font-medium pb-7 md:text-4xl'>PERSONAALNE • PÕHJALIK • PAINDLIK</div>
       <div><CTAButton3 /></div>  
     
   </section>

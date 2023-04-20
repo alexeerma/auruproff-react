275775714ea3ee2468c389c477bacbba308f8d7c
alexeerma <aleksander.eerma@hotmail.com>
2023-04-20 23:02:47 +0300

    Added contactform using emailjs, need styling

--- src/components/Contactform.jsx
+++ src/components/Contactform.jsx
@@ -1,9 +1,115 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 
-function Contactform() {
+const Contactform = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Conferelaw",
+          from_email: form.email,
+          to_email: "aleksander.eerma@hotmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
+  };
+
   return (
-    <div>Contactform</div>
-  )
-}
+    <section className="">
+        <div className=''>
+          <h1 className="">Kontakt</h1>
+        </div>
+        <div className=" ">
+          <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className=''>
+            <input
+              type='text'
+              name='name'
+              value={form.name}
+              onChange={handleChange}
+              placeholder="Ees- ja perekonnanimi"
+              className=''
+            />
+            <input
+              type='email'
+              name='email'
+              value={form.email}
+              onChange={handleChange}
+              placeholder="E-mail"
+              className=''
+            />
+            <textarea
+              rows={7}
+              name='message'
+              value={form.message}
+              onChange={handleChange}
+              placeholder='Teade'
+              className=''
+            />
+            <button
+            type='submit'
+            className=''
+          >
+            {loading ? "Sending..." : "Send"}
+          </button>
+          </form>
+          <div className=''>
+            <img src={logo_black} alt="" className="max-w-1/2"/>
+            <h4 className="">Advokaadibüroo</h4>
+            <p className='text-center'>Aadress: Tartu mnt 83, Tallinn, 10115, Eesti</p>
+            <p className='text-center'>Telefon: +372 6740433</p>
+            <p className='text-center'>Email: info@conferelaw.ee</p>
+          </div>
+        </div>
+      </section>
+
+    );
+  };
 
 export default Contactform

b65ec831bb846018439040b6bef6f4abe89e86f6
alexeerma <aleksander.eerma@hotmail.com>
2023-05-08 09:22:38 +0300

    Added images data

--- src/components/images.js
+++ src/components/images.js
@@ -2,24 +2,31 @@ const images = [
     {
       id: 1,
       title: 'Image 1',
-      category: 'Category 1',
+      category: 'Fassaad',
       thumbnail: 'https://via.placeholder.com/300x200',
       fullSize: 'https://via.placeholder.com/1200x800',
     },
     {
       id: 2,
       title: 'Image 2',
-      category: 'Category 2',
+      category: 'Betoon',
       thumbnail: 'https://via.placeholder.com/300x200',
       fullSize: 'https://via.placeholder.com/1200x800',
     },
     {
       id: 3,
       title: 'Image 3',
-      category: 'Category 3',
+      category: 'Siselahendused',
       thumbnail: 'https://via.placeholder.com/300x200',
       fullSize: 'https://via.placeholder.com/1200x800',
     },
+    {
+      id: 4, 
+      title: 'Image 4',
+      category: 'Erilahendused',
+      thumbnail: 'https://via.placeholder.com/300x200',
+      fullSize: 'https://via.placeholder.com/1200x800',  
+    }
     // add more images here...
   ];
   

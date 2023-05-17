40b59794234cebc44afcef1ccd9ad194cee6bfd6
alexeerma <aleksander.eerma@hotmail.com>
2023-05-17 12:57:32 +0300

    Changes to accordion

--- src/components/FAQAccordion.jsx
+++ src/components/FAQAccordion.jsx
@@ -5,8 +5,25 @@ import {
   AccordionBody,
 } from "@material-tailwind/react";
  
+function Icon({ id, open }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={`${
+        id === open ? "rotate-180" : ""
+      } h-5 w-5 transition-transform`}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+    </svg>
+  );
+}
+ 
 function FAQAccordion() {
-  const [open, setOpen] = useState(1);
+  const [open, setOpen] = useState(0);
  
   const handleOpen = (value) => {
     setOpen(open === value ? 0 : value);
@@ -14,33 +31,33 @@ function FAQAccordion() {
  
   return (
     <Fragment>
-      <Accordion open={open === 1}>
-        <AccordionHeader onClick={() => handleOpen(1)}>
+      <Accordion className="" open={open === 1} icon={<Icon id={1} open={open} />}>
+        <AccordionHeader className="text-center justify-around" onClick={() => handleOpen(1)}>
           What is Material Tailwind?
         </AccordionHeader>
-        <AccordionBody>
+        <AccordionBody className="text-center">
           We&apos;re not always in the position that we want to be at.
           We&apos;re constantly growing. We&apos;re constantly making mistakes.
           We&apos;re constantly trying to express ourselves and actualize our
           dreams.
         </AccordionBody>
       </Accordion>
-      <Accordion open={open === 2}>
-        <AccordionHeader onClick={() => handleOpen(2)}>
+      <Accordion open={open === 2} icon={<Icon id={2} open={open} />}>
+        <AccordionHeader className="justify-around"onClick={() => handleOpen(2)}>
           How to use Material Tailwind?
         </AccordionHeader>
-        <AccordionBody>
+        <AccordionBody className="text-center">
           We&apos;re not always in the position that we want to be at.
           We&apos;re constantly growing. We&apos;re constantly making mistakes.
           We&apos;re constantly trying to express ourselves and actualize our
           dreams.
         </AccordionBody>
       </Accordion>
-      <Accordion open={open === 3}>
-        <AccordionHeader onClick={() => handleOpen(3)}>
+      <Accordion open={open === 3} icon={<Icon id={3} open={open} />}>
+        <AccordionHeader className="justify-around" onClick={() => handleOpen(3)}>
           What can I do with Material Tailwind?
         </AccordionHeader>
-        <AccordionBody>
+        <AccordionBody className="text-center">
           We&apos;re not always in the position that we want to be at.
           We&apos;re constantly growing. We&apos;re constantly making mistakes.
           We&apos;re constantly trying to express ourselves and actualize our

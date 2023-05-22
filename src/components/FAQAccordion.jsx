d3d9453b2b0c3afca091329b1a9b98e9c6a68cc7
alexeerma <aleksander.eerma@hotmail.com>
2023-05-22 10:48:16 +0300

    Accordion data and styling

--- src/components/FAQAccordion.jsx
+++ src/components/FAQAccordion.jsx
@@ -33,10 +33,10 @@ function FAQAccordion() {
     <Fragment>
       <Accordion className="" open={open === 1} icon={<Icon id={1} open={open} />}>
         <div></div>
-        <AccordionHeader className="text-center justify-between" onClick={() => handleOpen(1)}>
-          What is Material Tailwind?
+        <AccordionHeader className="text-center text-primary uppercase justify-between" onClick={() => handleOpen(1)}>
+          Mis on ruutmeetri(M2) hind?
         </AccordionHeader>
-        <AccordionBody className="text-center">
+        <AccordionBody className="text-center text-primary ">
           We&apos;re not always in the position that we want to be at.
           We&apos;re constantly growing. We&apos;re constantly making mistakes.
           We&apos;re constantly trying to express ourselves and actualize our
@@ -44,10 +44,10 @@ function FAQAccordion() {
         </AccordionBody>
       </Accordion>
       <Accordion open={open === 2} icon={<Icon id={2} open={open} />}>
-        <AccordionHeader className="justify-around"onClick={() => handleOpen(2)}>
+        <AccordionHeader className="justify-between text-primary uppercase "onClick={() => handleOpen(2)}>
           How to use Material Tailwind?
         </AccordionHeader>
-        <AccordionBody className="text-center">
+        <AccordionBody className="text-center text-primary ">
           We&apos;re not always in the position that we want to be at.
           We&apos;re constantly growing. We&apos;re constantly making mistakes.
           We&apos;re constantly trying to express ourselves and actualize our
@@ -55,10 +55,10 @@ function FAQAccordion() {
         </AccordionBody>
       </Accordion>
       <Accordion open={open === 3} icon={<Icon id={3} open={open} />}>
-        <AccordionHeader className="justify-around" onClick={() => handleOpen(3)}>
+        <AccordionHeader className="justify-between text-primary uppercase" onClick={() => handleOpen(3)}>
           What can I do with Material Tailwind?
         </AccordionHeader>
-        <AccordionBody className="text-center">
+        <AccordionBody className="text-center text-primary ">
           We&apos;re not always in the position that we want to be at.
           We&apos;re constantly growing. We&apos;re constantly making mistakes.
           We&apos;re constantly trying to express ourselves and actualize our
